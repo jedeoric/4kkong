93cb5aa8b9d707030b0f0b3675d21ee01df3eb05
jede <jede@oric.org>
2016-09-24 16:49:40 +0200

    insert it in release directory

--- make.bat
+++ make.bat
@@ -1,1 +1,3 @@
-%OSDK%\bin\xa.exe -C -W -e error.txt -l xa_labels.txt -o 4kkong.com main.s -D__DATEBUILT__="%MYDATE%"  -DTARGET_TELEMON
+
+
+%OSDK%\bin\xa.exe -C -W -e error.txt -l xa_labels.txt -o release\4kkong.com main.s -D__DATEBUILT__="%MYDATE%"  -DTARGET_TELEMON
