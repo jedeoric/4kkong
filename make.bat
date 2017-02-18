8f92f95c702157758fca226405d8d407dadb7ab9
jede <jede@oric.org>
2017-02-18 13:21:14 +0100

    Correcting

--- make.bat
+++ make.bat
@@ -5,5 +5,5 @@ SET ORICUTRON="..\..\..\oricutron\"
 
 %OSDK%\bin\xa.exe -C -W -e error.txt -l xa_labels.txt -o release\usr\bin\4kkong main.s -D__DATEBUILT__="%MYDATE%"  -DTARGET_TELEMON
 IF "%1"=="NORUN" GOTO End
-copy release\4kkong %ORICUTRON%\usbdrive\usr\bin
+copy release\usr\bin\4kkong %ORICUTRON%\usbdrive\usr\bin
 :End
