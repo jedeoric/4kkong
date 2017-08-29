bbc728149350b5bd727da916fa025aa985307441
jede <jede@oric.org>
2017-08-29 23:46:06 +0200

    Correcting man pages

--- make.bat
+++ make.bat
@@ -16,6 +16,7 @@ del /F /S /Q build\
 mkdir build\usr\share\doc\
 mkdir build\usr\share\doc\%BINARYFILE%
 mkdir build\usr\share\man
+mkdir build\usr\share\man\%BINARYFILE%
 mkdir build\usr\bin\
 
 IF NOT EXIST build\usr\share\ipkg mkdir build\usr\share\ipkg      
@@ -27,9 +28,17 @@ echo Building ipkg csv
 echo 4kkong    1.0.0  Kong game > src\ipkg\%BINARYFILE%.csv
 echo Copy README.md
 copy README.md build\usr\share\doc\%BINARYFILE%
+copy src\ipkg\%BINARYFILE%.csv build\usr\share\ipkg 
+copy src\man\%BINARYFILE%.hlp build\usr\share\man\
 
 
 %OSDK%\bin\xa.exe -C -W -e error.txt -l xa_labels.txt -o build\usr\bin\4kkong main.s -D__DATEBUILT__="%MYDATE%"  -DTARGET_TELEMON
 IF "%1"=="NORUN" GOTO End
+mkdir %ORICUTRON%\usbdrive\usr\share\man\%BINARYFILE%\
 copy build\usr\bin\4kkong %ORICUTRON%\usbdrive\usr\bin
+copy src\man\%BINARYFILE%.hlp %ORICUTRON%\usbdrive\usr\share\man\
+cd %ORICUTRON%
+OricutronV4 -mt -d disks/demo/OricTech.dsk
+cd %ORIGIN_PATH%
+
 :End
